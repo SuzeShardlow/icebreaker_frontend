6e734dd898b8c80fca14869ac8f41f329a6fd5c4
SuzeShardlow <suze.shardlow1@gmail.com>
2017-10-10 18:00:11 +0100

    Prepared client files for heroku deployment

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,4 @@
 angular
   .module('icebreaker')
-  .constant('API', `http://localhost:3000/api`);
+  // .constant('API', `http://localhost:3000/api`);
+  .constant('API', 'https://intense-stream-50021.herokuapp.com/api');
